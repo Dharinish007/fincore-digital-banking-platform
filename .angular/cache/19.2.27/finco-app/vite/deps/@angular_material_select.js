db57f0dcfdfad7aedfb546fe55f304e4b0ad5309
manikandan110306 <manit110306@gmail.com>
2026-08-05 19:00:49 +0530

    Balance Accuracy Frontend

---
+++ .angular/cache/19.2.27/finco-app/vite/deps/@angular_material_select.js
@@ -0,0 +1,57 @@
+import {
+  MAT_SELECT_CONFIG,
+  MAT_SELECT_SCROLL_STRATEGY,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER_FACTORY,
+  MAT_SELECT_TRIGGER,
+  MatSelect,
+  MatSelectChange,
+  MatSelectModule,
+  MatSelectTrigger,
+  matSelectAnimations
+} from "./chunk-JQEJGICL.js";
+import {
+  MatError,
+  MatFormField,
+  MatHint,
+  MatLabel,
+  MatPrefix,
+  MatSuffix
+} from "./chunk-M54Z4DBK.js";
+import "./chunk-5Z5ISBN7.js";
+import "./chunk-RNPDT76W.js";
+import "./chunk-MEIJUGNZ.js";
+import "./chunk-JHXNGOQI.js";
+import "./chunk-6VVYZFK4.js";
+import "./chunk-NPG2GJB4.js";
+import {
+  MatOptgroup,
+  MatOption
+} from "./chunk-O3NOPGGP.js";
+import "./chunk-QWC2HM52.js";
+import "./chunk-A6TVUFFE.js";
+import "./chunk-JNTZAZAZ.js";
+import "./chunk-TN23HS4Y.js";
+import "./chunk-2TAPZVZ4.js";
+import "./chunk-2YGPWXPO.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  MAT_SELECT_CONFIG,
+  MAT_SELECT_SCROLL_STRATEGY,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER_FACTORY,
+  MAT_SELECT_TRIGGER,
+  MatError,
+  MatFormField,
+  MatHint,
+  MatLabel,
+  MatOptgroup,
+  MatOption,
+  MatPrefix,
+  MatSelect,
+  MatSelectChange,
+  MatSelectModule,
+  MatSelectTrigger,
+  MatSuffix,
+  matSelectAnimations
+};
