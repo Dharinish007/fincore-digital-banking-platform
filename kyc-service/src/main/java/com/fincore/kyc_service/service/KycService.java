efb34a4118f7e291adaeb2b65069be237daa1fd0
TSathiyapriya30 <tsathiyapriya30@gmail.com>
2026-08-06 21:48:37 +0530

    Integrated KYC backend approval/rejection APIs

--- kyc-service/src/main/java/com/fincore/kyc_service/service/KycService.java
+++ kyc-service/src/main/java/com/fincore/kyc_service/service/KycService.java
@@ -1,9 +1,13 @@
 package com.fincore.kyc_service.service;
 
+import java.util.List;
+
 import com.fincore.kyc_service.dto.KycRequestDTO;
 import com.fincore.kyc_service.dto.KycResponseDTO;
 
 public interface KycService {
+
+    List<KycResponseDTO> getAllKyc();
 
     KycResponseDTO submitKyc(KycRequestDTO request);
 
