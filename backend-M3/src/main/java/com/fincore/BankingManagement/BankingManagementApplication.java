9277ea405719f3edf67509b72c38248add5ab9fd
NimmanaJeevanaJyothi <divyanimmana777@gmail.com>
2026-08-19 19:38:47 +0530

    Milestone 3 backend

---
+++ backend-M3/src/main/java/com/fincore/BankingManagement/BankingManagementApplication.java
@@ -0,0 +1,12 @@
+package com.fincore.BankingManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingManagementApplication.class, args);
+    }
+}
