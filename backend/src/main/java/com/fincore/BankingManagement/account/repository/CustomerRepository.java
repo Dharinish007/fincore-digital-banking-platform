74052cb89af4874c14d81c9a88046515a2445d56
Manikandan T <manit110306@gmail.com>
2026-08-20 18:18:08 +0530

    Delete backend directory

--- backend/src/main/java/com/fincore/BankingManagement/account/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fincore.BankingManagement.account.repository;
-
-import com.fincore.BankingManagement.Entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository  extends JpaRepository<Customer, Long> {
-    boolean existsByMobileNumber(String  mobileNumber);
-    boolean existsByEmail(String  email);
-}
