5a72ead4157343d4b3d4b49e455f356dc749305a
TSathiyapriya30 <tsathiyapriya30@gmail.com>
2026-08-22 15:59:44 +0530

    Add settlement confirmation backend

---
+++ src/main/java/com/fincore/settlement_confirmation_service/SettlementConfirmationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fincore.settlement_confirmation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettlementConfirmationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettlementConfirmationServiceApplication.class, args);
+	}
+
+}
