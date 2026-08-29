8ddc700d7e777ad4337c36be4b9e5e5274ce7890
Nithish Rapaka <rapakanithish96@gmail.com>
2026-08-29 14:13:16 +0530

    Removed files

--- payment-initiation/src/app/payment-initiation/models/fraud-check.model.ts
+++
@@ -1,9 +0,0 @@
-export interface FraudCheck {
-  fraud_check_id?: number;
-  payment_id: number;
-  risk_score: number;
-  fraud_status: 'Pending' | 'Safe' | 'Suspicious' | 'Blocked';
-  rule_triggered?: string;
-  remarks?: string;
-  checked_at?: string;
-}
