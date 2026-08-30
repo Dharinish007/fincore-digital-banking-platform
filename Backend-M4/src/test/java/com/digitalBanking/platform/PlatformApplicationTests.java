c9f56604a0c058ac87ee36b1ff616f5ef5837e0e
Nithish Rapaka <rapakanithish96@gmail.com>
2026-08-30 20:53:59 +0530

    Added Backed

---
+++ Backend-M4/src/test/java/com/digitalBanking/platform/PlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalBanking.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
