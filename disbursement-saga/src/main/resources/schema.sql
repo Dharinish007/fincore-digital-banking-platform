2ffebd624d936418536c998a719b6cf7b7bfbda2
SharvariShalgar20 <shalgarsharvari@gmail.com>
2026-08-31 16:02:43 +0530

    chore

--- disbursement-saga/src/main/resources/schema.sql
+++ disbursement-saga/src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS disbursement_saga_db;
 
-USE disbursement_saga_db;
+Use disbursement_saga_db;
 
-SHOW Tables;
+SHOW tables;
 
 SELECT * FROM disbursement_saga;
--- notification-service/src/main/resources/schema.sql
+++ notification-service/src/main/resources/schema.sql
@@ -1,1 +1,7 @@
 CREATE DATABASE IF NOT EXISTS bankingSystem_notification_db;
+
+Use bankingSystem_notification_db;
+
+SHOW tables;
+
+SELECT * FROM notifications;
