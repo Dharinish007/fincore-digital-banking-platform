1a08b433359cdb0a70e295d9997225ebd0a42e0d
NimmanaJeevanaJyothi <divyanimmana777@gmail.com>
2026-09-01 15:45:55 +0530

    Updated platform application configuration

--- Backend-M4/src/main/java/com/digitalBanking/platform/PlatformApplication.java
+++ Backend-M4/src/main/java/com/digitalBanking/platform/PlatformApplication.java
@@ -2,12 +2,15 @@ package com.digitalBanking.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.digitalBanking")
+@EntityScan("com.digitalBanking")
+@EnableJpaRepositories("com.digitalBanking")
 public class PlatformApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PlatformApplication.class, args);
-
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformApplication.class, args);
+    }
 }
